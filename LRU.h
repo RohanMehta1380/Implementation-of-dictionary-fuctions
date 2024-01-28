f4827aca72f7e415c5ca60d3b0f3c4bb3e693035
RohanMehta1380 <59866922+RohanMehta1380@users.noreply.github.com>
2024-01-28 14:38:54 +0100

    Update LRU.h

--- LRU.h
+++ LRU.h
@@ -1,126 +1,64 @@
-#include<iostream>
-#include<string>
-#include<stdlib.h>
-#include<vector>
-#include<fstream>
-#include<unistd.h>
-#include<conio.h>
-#define ALPHABETS 26
+// #ifndef LRU_H
+// #define LRU_H
+
+// #include <string>
+// #include <list>
+// #include <unordered_map>
+// using namespace std;
+
+// class LRUCache {
+// private:
+//     list<string> lruList;
+//     unordered_map<string, list<string>::iterator> lruMap;
+
+// public:
+//     void put(string word) {
+//         if (lruMap.find(word) != lruMap.end()) {
+//             lruList.erase(lruMap[word]);
+//         }
+//         lruList.push_front(word);
+//         lruMap[word] = lruList.begin();
+//     }
+
+//     string getLRUWord() {
+//         return lruList.back();
+//     }
+// };
+
+// #endif // LRU_H
+
+#ifndef LRU_H
+#define LRU_H
+
+#include <string>
+#include <list>
+#include <unordered_map>
 using namespace std;
-class LRUCache
-{
+
+class LRUCache {
 private:
-    /*Node for each key*/
-    struct LRUNode
-    {
-        int key, val;
-        struct LRUNode *prev, *next;
-        LRUNode() : prev(NULL), next(NULL) {}
-        LRUNode(int key, int val) : key(key), val(val), prev(NULL), next(NULL) {}
-    };
-
-int cap;                                //Capacity of LRU cache
-    int curCnt;                         //Current cnt of nodes
-    unordered_map<int, LRUNode *> M;    //Map from key to corresponding node
-
-    LRUNode *oldNode, *newNode;         //Oldest and newest LRU node
-
-    /*Adjust paramters after current query for key*/
-    void curQuery(int key)
-    {
-        LRUNode *temp = M[key];
-        if (temp == newNode)
-            return;
-        LRUNode *prev = temp->prev;
-        LRUNode *next = temp->next;
-        if (temp == oldNode)
-        {
-            oldNode = next;
-            next->prev = NULL;
-            newNode->next = temp;
-            temp->prev = newNode;
-            temp->next = NULL;
-            newNode = temp;
-        }
-        else
-        {
-            prev->next = next;
-            next->prev = prev;
-            newNode->next = temp;
-            temp->prev = newNode;
-            temp->next = NULL;
-            newNode = temp;
-        }
-        return;
-    }
-
-    /*Add key*/
-    void addKey(int key)
-    {
-        if (!newNode)
-        {
-            newNode = M[key];
-            oldNode = newNode;
-            return;
-        }
-        LRUNode *temp = M[key];
-        newNode->next = temp;
-        temp->prev = newNode;
-        newNode = temp;
-        return;
-    }
-
-    /*Removes oldest query in cache*/
-    void removeOldest()
-    {
-        curCnt--;
-        int key = oldNode->key;
-        if (oldNode == newNode)
-        {
-            M.erase(key);
-            oldNode = newNode = NULL;
-            return;
-        }
-        oldNode = oldNode->next;
-        oldNode->prev = NULL;
-        M.erase(key);
-        return;
-    }
+    list<string> lruList;
+    unordered_map<string, list<string>::iterator> lruMap;
+    int capacity;
 
 public:
-    LRUCache(int capacity)
-    {
-        cap = capacity;
-        curCnt = 0;
-        oldNode = NULL;
-        newNode = NULL;
+    LRUCache(int cap) : capacity(cap) {}
+
+    void put(string word) {
+        if (lruMap.find(word) != lruMap.end()) {
+            lruList.erase(lruMap[word]);
+        } else if (lruList.size() == capacity) {
+            string leastRecent = lruList.back();
+            lruList.pop_back();
+            lruMap.erase(leastRecent);
+        }
+        lruList.push_front(word);
+        lruMap[word] = lruList.begin();
     }
 
-    int get(int key)
-    {
-        if (M.find(key) != M.end())
-        {
-            curQuery(key);
-            return M[key]->val;
-        }
-        return -1;
-    }
-
-    void put(int key, int value)
-    {
-        if (M.find(key) != M.end())
-        {
-            M[key]->val = value;
-            curQuery(key);
-        }
-        else
-        {
-            LRUNode *temp = new LRUNode(key, value);
-            M[key] = temp;
-            if (curCnt == cap)
-                removeOldest();
-            curCnt++;
-            addKey(key);
-        }
+    string getLRUWord() {
+        return lruList.back();
     }
 };
+
+#endif // LRU_H
